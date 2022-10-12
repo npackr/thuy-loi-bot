86416b7d7ba2216497582332ed51d7c0e748af19
Mã Đằng <mateng@npackr.com>
2022-10-12 17:54:06 +0700

    V2022.10.12.5: Tối ưu hiển thị mục Nhập học

--- src/functions/commands/register.js
+++ src/functions/commands/register.js
@@ -5,9 +5,10 @@ export async function register(interaction, user, options) {
   const string = getString(user.language);
   const configurations = options.configurations;
   const allowRegister = configurations.find(setting => setting.name === 'allowRegister').value;
-  if (!allowRegister) { return await interaction.editReply({ content: `Hiện đã hết thời gian đăng ký nhập học, vui lòng liên hệ <@${string.ADMIN_ID}> để được hỗ trợ!`, ephemeral: true }); }
-
-  const addmissButton = new ButtonBuilder().setLabel('Đăng ký nhập học').setStyle(ButtonStyle.Link).setURL('https://nhaphoc.tlu.edu.vn/');
+  if (!allowRegister) { return await interaction.editReply({ content: `${string.REGISTER_TIMEOUT}`, ephemeral: true }); }
+  
+  const addmissButton = new ButtonBuilder().setLabel(string.GO_REGISTER).setStyle(ButtonStyle.Link).setURL('https://nhaphoc.tlu.edu.vn/');
   const addmissionOptions = new ActionRowBuilder().addComponents(addmissButton);
-  return await interaction.editReply({ content: `Chào mừng Tân sinh viên K64 của ${string.SCHOOL_NAME}!`, components: [addmissionOptions], ephemeral: true });
+  const stringHeader = `${string.SCHOOL_LOGO}\n**${string.NEW_STUDENT_REGISTER.toUpperCase()}**\n`;
+  return await interaction.editReply({ content: `${stringHeader}${string.WELCOME_TO_REGISTER}!\n\n${string.REGISTER_DESCRIPTION}`, components: [addmissionOptions], ephemeral: true });
 }

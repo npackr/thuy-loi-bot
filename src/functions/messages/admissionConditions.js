e3149e6225d7c44a01366daf4d6bd4d5c1b8c5e7
Mã Đằng <mateng@npackr.com>
2022-10-19 20:47:26 +0700

    V2022.10.19

--- src/functions/messages/admissionConditions.js
+++ src/functions/messages/admissionConditions.js
@@ -1,9 +1,17 @@
 export function checkAdmissionConditions(messageContent) {
   switch (messageContent) {
-    case 'đăng ký': return true;
-    case 'dang ky': return true;
-    case 'nhập học': return true;
-    case 'nhap hoc': return true;
+    case 'tuyển sinh': return true;
+    case 'tuyen sinh': return true;
+    case 'xet hoc ba': return true;
+    case 'xét học bạ': return true;
+    case 'nguyen vong': return true;
+    case 'nguyện vọng': return true;
+    case 'xet tuyen': return true;
+    case 'xét tuyển': return true;
+    case 'tsinh': return true;
+    case 'tuyensinh': return true;
+    case 'Tuyen sinh': return true;
+    case 'Tuyển sinh': return true;
     default:
       return false;
   }
--- src/functions/messages/registerConditions.js
+++ src/functions/messages/registerConditions.js
@@ -1,17 +1,9 @@
 export function checkRegisterCondition(messageContent) {
-  switch (messageContent) {
-    case 'tuyển sinh': return true;
-    case 'tuyen sinh': return true;
-    case 'xet hoc ba': return true;
-    case 'xét học bạ': return true;
-    case 'nguyen vong': return true;
-    case 'nguyện vọng': return true;
-    case 'xet tuyen': return true;
-    case 'xét tuyển': return true;
-    case 'tsinh': return true;
-    case 'tuyensinh': return true;
-    case 'Tuyen sinh': return true;
-    case 'Tuyển sinh': return true;
+  switch (messageContent) {\
+    case 'đăng ký': return true;
+    case 'dang ky': return true;
+    case 'nhập học': return true;
+    case 'nhap hoc': return true;
     default:
       return false;
   }

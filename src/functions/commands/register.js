58a474431db4fa0fcd87ed94896d9ad174136071
Mã Đằng <mateng@npackr.com>
2022-11-02 14:48:08 +0700

    V2022.11.02.2

--- src/functions/commands/register.js
+++ src/functions/commands/register.js
@@ -1,14 +1,28 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, SelectMenuBuilder } from "discord.js";
+import { cooldownTime } from "../../../deploy-commands.js";
+import { getCategories } from "../database/queries/common/getCategories.js";
 import { getString } from "../languages/stringRouter.js";
 
 export async function register(interaction, user, options) {
   const string = getString(user.language);
   const configurations = options.configurations;
   const allowRegister = configurations.find(setting => setting.name === 'allowRegister').value;
-  if (!allowRegister) { return await interaction.editReply({ content: `${string.REGISTER_TIMEOUT}`, ephemeral: true }); }
-  
-  const addmissButton = new ButtonBuilder().setLabel(string.GO_REGISTER).setStyle(ButtonStyle.Link).setURL('https://nhaphoc.tlu.edu.vn/');
-  const addmissionOptions = new ActionRowBuilder().addComponents(addmissButton);
-  const stringHeader = `${string.SCHOOL_LOGO}\n**${string.NEW_STUDENT_REGISTER.toUpperCase()}**\n`;
-  return await interaction.editReply({ content: `${stringHeader}${string.WELCOME_TO_REGISTER}!\n\n${string.REGISTER_DESCRIPTION}`, components: [addmissionOptions], ephemeral: true });
+  const stringHeader = `${string.SCHOOL_LOGO}\n**${string.NEW_STUDENT_REGISTER.toUpperCase()}**\n\n`;
+  if (!allowRegister) { return await interaction.editReply({ content: `${stringHeader}${string.REGISTER_TIMEOUT}`, ephemeral: true }); }
+  const categories = await getCategories(2);
+  const selectMenu = new SelectMenuBuilder().setCustomId('select_register_category').setPlaceholder(string.SELECT_CATEGORIES).addOptions(categories.map(category => {
+    return { label: category.name, value: category.id.toString(), description: category.description }
+  }));
+  const categoryRow = new ActionRowBuilder().addComponents(selectMenu);
+  await interaction.editReply({ content: `${stringHeader}`, components: [categoryRow] });
+
+  const filter = i => i.customId === 'select_register_category' && i.user.id === interaction.user.id;
+  const collector = interaction.channel.createMessageComponentCollector({ filter, time: cooldownTime });
+  collector.on('collect', async i => {
+    await i.update({ content: string.LOADING_STRING, components: [] }).catch(console.error);
+    const category = categories.find(category => category.id.toString() === i.values[0]);
+    const registerButton = new ButtonBuilder().setLabel(category.url_name).setStyle(ButtonStyle.Link).setURL(category.url);
+    const registerOptions = new ActionRowBuilder().addComponents(registerButton);
+    return await interaction.editReply({ content: `${stringHeader}${category.content}`, components: [registerOptions], ephemeral: true });
+  });
 }
